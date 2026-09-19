953d9c221d04d4b64f937d0b88d191885217eb6a
sampadaprajwal <sampadaprajwalbhat@gmail.com>
2026-09-19 13:38:53 +0530

    Update cancer patient data.py

--- cancer patient data.py
+++ cancer patient data.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-<<<<<<< HEAD
-dscdvfertgtrgbtfbr
-=======
-adwdwwew
->>>>>>> 74a340a097acdec5de2565e1bb6329f240bbe4c2
+import sklearn
+print(pd.__version__)
+print(np.__version__)
+print(sklearn.__version__)
+print(matplotlib.__version__)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_curve, roc_auc_score
